6e2ffabbe04ac8363206defe726cadadb02997fd
nerychapeton <nerychapeton@gmail.com>
2018-02-16 09:27:55 -0800

    JUnit, interface, and readme
    Added JUnit tester for sprint 1 and 2
    Added interface BigNumber with TODO list/reminders
    Updated README with small changes about TODO list/reminders

---
+++ src/edu/miracosta/cs113/BigNumber.java
@@ -0,0 +1,19 @@
+package edu.miracosta.cs113;
+
+public interface BigNumber {
+
+    /*TODO:
+     * - make sure to document interface methods below!!!
+     * - these are specific to Sprint 2, so for Sprint 1 you can simply have the methods throw an UnsupportedOperationException
+     * - do not forget to consult your UML for each sprint so you know ALL methods to build (interface
+     * does not contain constructors and toString methods! don't forget!!)
+     */
+
+    public void add(int n);
+
+    public void add(String n);
+
+    public void subtract(int n);
+
+    public void subtract(String n);
+}
