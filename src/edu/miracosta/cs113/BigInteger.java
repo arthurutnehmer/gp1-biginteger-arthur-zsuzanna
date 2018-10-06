c6e0f67f93f8962520d1b32a4f4f85284c0c9f7e
Zsuzsanna Dianovics <31230409+deverzs@users.noreply.github.com>
2018-10-06 15:33:17 -0700

    Initial Class implementation

---
+++ src/edu/miracosta/cs113/BigInteger.java
@@ -0,0 +1,37 @@
+package edu.miracosta.cs113;
+
+import java.util.LinkedList;
+
+public class BigInteger implements BigNumber {
+
+    private LinkedList<Integer> bigInt ;
+    private boolean negative ;
+
+    public BigInteger(String n) {
+
+    }
+
+    public BigInteger() {
+
+    }
+
+    @Override
+    public void add(int n) {
+
+    }
+
+    @Override
+    public void add(String n) {
+
+    }
+
+    @Override
+    public void subtract(int n) {
+
+    }
+
+    @Override
+    public void subtract(String n) {
+
+    }
+}
