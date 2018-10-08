4f9e3b43da268faff99e79fe7072001e709b3853
Arthur Utnehmer <arthurutnehmer@gmail.com>
2018-10-08 12:05:05 -0700

    added huge number.

--- src/edu/miracosta/cs113/Test.java
+++ src/edu/miracosta/cs113/Test.java
@@ -1,13 +1,33 @@
 package edu.miracosta.cs113;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.util.function.BiFunction;
 
 public class Test
 {
     public static void main(String[]args)
     {
-        BigInteger test1 = new BigInteger();
-        test1.add("99,999,999,999,999,999,999,999,999,999,999,999,999,999,999,999,999");
-        test1.add("1");
-        System.out.println(test1);
+        BigInteger largeNumber = new BigInteger();
+        try {
+        File file = new File("src/edu/miracosta/cs113/Largesum.txt");
+        FileReader fileReader = new FileReader(file);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        StringBuffer stringBuffer = new StringBuffer();
+        String line;
+        while ((line = bufferedReader.readLine()) != null)
+        {
+            largeNumber.add(line);
+        }
+        fileReader.close();
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+
+    System.out.println(largeNumber);
+
+
 
     }
 }
